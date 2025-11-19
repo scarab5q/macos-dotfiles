337a6b43ef0c49ad806f7619072391476cb3cd55
Jack Michael Denny <scarab5q@Jacks-MacBook-Pro.local>
2025-11-19 15:53:13 +0000

    initial commit

---
+++ .config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+map("n", ";", ":")
+map("n", ":", ";")
+map("n", "U", "<c-r>")
+map("n", "Q", "@q")
---
+++ .zprofile
@@ -0,0 +1,4 @@
+eval "$(/opt/homebrew/bin/brew shellenv)"
+export NPM_TOKEN="REDACTED_NPM_TOKEN"
+export EDITOR="nvim"
+export PATH=~/scripts:$PATH
