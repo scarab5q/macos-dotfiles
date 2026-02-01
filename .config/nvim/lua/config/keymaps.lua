2ab29567a1a227df8b9a3e4f100be0a28ae8e0ab
Jack Denny <jackmdenny@gmail.com>
2026-02-01 15:12:28 +0000

    add config files keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -4,7 +4,25 @@
 
 local map = vim.keymap.set
 
-map("n", ";", ":")
-map("n", ":", ";")
+-- Delete any existing ; and : mappings first to ensure our swap takes precedence
+pcall(vim.keymap.del, "n", ";")
+pcall(vim.keymap.del, "n", ",")
+
+map("n", ";", ":", { desc = "Command mode" })
+map("n", ":", ";", { desc = "Repeat f/t" })
 map("n", "U", "<c-r>")
 map("n", "Q", "@q")
+
+-- Find files in dotfiles (bare git repo at ~/.cfg)
+map("n", "<leader>Cf", function()
+  Snacks.picker.files({
+    cmd = "git",
+    args = {
+      "--git-dir=" .. os.getenv("HOME") .. "/.cfg",
+      "--work-tree=" .. os.getenv("HOME"),
+      "ls-files",
+    },
+    cwd = os.getenv("HOME"),
+    title = "Dotfiles",
+  })
+end, { desc = "Find dotfiles" })
