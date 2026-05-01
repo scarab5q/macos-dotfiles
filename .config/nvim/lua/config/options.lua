9cbf98f2ad2e2b705f9261179c25b88b0fd977d8
Jack Denny <24301052+scarab5q@users.noreply.github.com>
2026-05-01 14:29:59 +0100

    update neovim settings

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Monorepo: prefer .git as project root over LSP, so file pickers search the
+-- entire repo instead of just apps/arq, apps/backend, etc.
+vim.g.root_spec = { { ".git" }, "lsp", "cwd" }
